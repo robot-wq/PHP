177b78e791d83e83a2e005983531e4c795a05340
Takeshi <yoshikawapaulo46@gmail.com>
2026-09-10 15:19:43 -0300

    Add files via upload

---
+++ 3_tipo_de_dados/10_booleanos/index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo True;
+echo "<br>";
+echo false;
+
+if(True){
+    echo "É um verdadeiro <br>";
+}
+
+if(5>2){
+    echo "É verdadeiro! <br>";
+}
+
+$podeentrar = True;
+
+if($podeentrar){
+    echo "O   usuário pode entrar <br>";
+}
---
+++ 3_tipo_de_dados/4_float/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = 1.12;
+
+echo $a;
+echo "<br>";
+echo 1.123; 
+echo "<br>";
+echo 12.4 + 1.3279;
